10d3d76a16eefc131184cfe17851c2c75e331597
Lara <124082870+larahsan@users.noreply.github.com>
2025-06-14 22:05:25 +0200

    Add files via upload

---
+++ js/juego.js
@@ -0,0 +1,174 @@
+class Juego {
+
+    constructor() {
+        this.preguntas = [
+            "¿Qué comida tradicional puedes encontrar en Teverga?",
+            "¿Cuál es una de las rutas de senderismo más conocidas en Teverga?",
+            "¿Que producto artesanal es originario de Teverga?",
+            "¿En qué zona de Asturias se encuentra Teverga?",
+            "¿Qué ingrediente NO llevan las casadiellas de Teverga?",
+            "¿Desde qué ruta en Teverga podemos ver el Pico Caldoveiro?",
+            "¿Cuál de estos postres es típico de Teverga?",
+            "¿Cuál de estas es una famosa atracción en Teverga?",
+            "¿De qué forma se puede preparar la fabada tradicionalmente en Teverga?",
+            "¿Cual de estas rutas NO pertenece al concejo de Teverga?"
+        ];
+
+        this.respuestas = [
+            [
+                "Pote de castañas",
+                "Fabes con almejas",
+                "Cachopo de merluza",
+                "Pastel de cabracho",
+                "Repollo relleno"
+            ],
+            [
+                "Senda del Oso",
+                "Ruta del Cares",
+                "Camino Primitivo",
+                "Ruta de las Xanas",
+                "Senda Costera"
+            ],
+            [
+                "Queso de fuente",
+                "Sidra natural",
+                "Miel de brezo",
+                "Jabón de lavanda",
+                "Cerámica de Cudillero"
+            ],
+            [
+                "Sur",
+                "Norte",
+                "Este",
+                "Oeste",
+                "Centro"
+            ],
+            [
+                "Mantequilla",
+                "Harina",
+                "Nuez",
+                "Azúcar",
+                "Anís"
+            ],
+            [
+                "Puertos de Marabio",
+                "Senda del Oso",
+                "Las brañas de Vicenturo y Cueiro",
+                "Carmín Real de la Mesa",
+                "Ruta Vaqueira"
+            ],
+            [
+                "Arroz con leche",
+                "Tarta de Santiago",
+                "Frixuelos rellenos",
+                "Carbayones",
+                "Leche frita"
+            ],
+            [
+                "El Parque de la Prehistoria",
+                "Museo del Jurásico",
+                "Teleférico de Fuente Dé",
+                "Parque Natural de Somiedo",
+                "La Cuevona de Cuevas"
+            ],
+            [
+                "Con calamares",
+                "Con compango",
+                "Con chorizo y morcilla",
+                "Con fabes verdinas",
+                "Con pimientos del piquillo"
+            ],
+            [
+                "Ruta del Alba",
+                "Ruta de la Braña de las Cadenas",
+                "Ruta de la Braña de Cuevas",
+                "Ruta de Navariegas",
+                "Ruta del Urogallo"
+            ]
+        ]
+
+        document.querySelector('main').innerHTML = `
+            <article>
+                <h3></h3>
+                <label></label>
+                <label></label>
+                <label></label>
+                <label></label>
+                <label></label>
+                <button type="button" disabled>Siguiente</button>
+            </article>`
+
+        this.index = 0;
+        this.puntuacion = 0;
+        this.mostrarPregunta();
+    }
+
+    mostrarPregunta() {
+        const article = document.querySelector('main article');
+        const siguiente = article.querySelector('button');
+
+        // Pregunta
+        article.querySelector('h3').textContent = `${this.index+1}. ${this.preguntas[this.index]}`;
+
+        // Asignar el valor a cada respuesta (0, 1, 2, 3, 4)
+        const opciones = this.respuestas[this.index].map((texto, i) => ({
+            texto,
+            value: i
+        }));
+
+        // Barajar las opciones
+        for (let i = opciones.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [opciones[i], opciones[j]] = [opciones[j], opciones[i]];
+        }
+
+        // Añadir las labels barajadas
+        const labels = article.querySelectorAll('label');
+        for (let i = 0; i < labels.length; i++)
+            labels[i].innerHTML = `<input type="radio" name="pregunta" value="${opciones[i].value}"/> ${opciones[i].texto}`;
+
+        // Habilitar el botón de siguiente sólo si hay una respuesta seleccionada
+        article.querySelectorAll('input').forEach(input => {
+            input.addEventListener('change', () => {
+                siguiente.disabled = false;
+            });
+        });
+
+        // Pasar a la siguiente pregunta
+        siguiente.onclick = () => this.procesarRespuesta();
+    }
+
+    procesarRespuesta() {
+        // Hasta que no se seleccione una respuesta está deshabilitado
+        document.querySelector('main article button').disabled = true;
+
+        const seleccion = document.querySelector('input:checked');
+        if (parseInt(seleccion.value) === 0) // La correcta siempre es la primera
+            this.puntuacion++;
+
+        this.index++;
+        if (this.index >= this.preguntas.length) 
+            this.finJuego();
+        else 
+            this.mostrarPregunta();
+    }
+
+    finJuego() {
+        const article = document.querySelector('main article');
+
+        let mensaje = '';
+        if(this.puntuacion < 5)
+            mensaje = '¡Vaya! Has perdido.';
+        else
+            mensaje = '¡Felicidades! Has ganado.'
+
+        article.innerHTML = `
+            <h3>${mensaje} Tu puntuación es: ${this.puntuacion}/${this.preguntas.length}.</h3>
+            <button>Jugar de nuevo</button>
+        `;
+
+        article.querySelector('button').onclick = () => {
+            new Juego();
+        };
+    }
+}
---
+++ xml/xml2perfil.py
@@ -0,0 +1,51 @@
+import xml.etree.ElementTree as ET
+
+arbol = ET.parse('xml/rutas.xml')
+raiz = arbol.getroot()
+ns = {'u': 'http://www.uniovi.es'} 
+
+def obtenerCoordenadas():
+
+    color = ['red', 'blue', 'green']
+    for idx, ruta in enumerate(raiz.findall('u:ruta', ns), start=1):
+        nombre_ruta = ruta.find('u:info', ns).get('nombre')
+        coordenadas_por_ruta = [('Inicio', 0, float(ruta.find('u:localizacion/u:coordenadas', ns).get('altitud')))]
+
+        for hito in ruta.findall('u:hito', ns):
+            distancia = float(hito.find('u:distanciaDesdeHito', ns).get('unidades'))
+            altitud = float(hito.find('u:coordenadas', ns).get('altitud'))
+            nombreHito = hito.get('nombreHito')
+            coordenadas_por_ruta.append((nombreHito, distancia, altitud))
+
+        crearSvg(nombre_ruta, coordenadas_por_ruta, idx, color[idx-1])
+
+def crearSvg(nombre_ruta, coordenadas_por_ruta, idx, color):
+    svg_header = '''<?xml version="1.0" encoding="UTF-8"?>
+                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1">'''
+
+    contenido_svg = [svg_header]
+    x = 10
+    puntos = [f"{x},160"]
+    contenido_svg.append(f'<text x="10" y="20" font-size="20" fill="black">{nombre_ruta}</text>')
+    contenido_svg.append('<g transform="translate(0,30)">')
+
+    x_actual = x
+    for i, (nombreHito, dist, alt) in enumerate(coordenadas_por_ruta):
+        x_actual += dist // 18
+        y_actual = 160 - alt / 15
+        puntos.append(f"{x_actual},{y_actual}")
+        contenido_svg.append(f'<circle cx="{x_actual}" cy="{y_actual}" r="3" fill="{color}"/>')
+        offset = 20 if i % 2 == 0 else 40
+        contenido_svg.append(f'<text x="{x_actual}" y="{y_actual - offset}" font-size="10" fill="black">{i}: {nombreHito}</text>')
+
+    puntos.append(f"{x_actual},160 10,160")
+    puntos_str = " ".join(puntos)
+    contenido_svg.append(f'<polyline points="{puntos_str}" style="fill:white;stroke:{color};stroke-width:2" />')
+    contenido_svg.append('</g>\n</svg>')
+
+    output_file = f'xml/svg/altimetria{idx}.svg'
+    with open(output_file, 'w', encoding="utf-8") as f:
+        f.write('\n'.join(contenido_svg))
+
+if __name__ == "__main__":
+    obtenerCoordenadas()
